5f09a17bedf6ca166d7a12bc46862bb117d2f9b7
Tomas Cordero <tomas.cordero@corp.rrpartners.com>
2018-05-11 18:42:14 -0700

    added logic

--- bin/global.js
+++ bin/global.js
@@ -2,8 +2,8 @@
 
 let fs = require('fs');
 let program = require('commander');
-let co = require('co');
-let prompt = require('co-prompt');
+let inquirer = require('inquirer');
+let colors = require('colors');
 
 program.version('0.0.1', '-v, --version')
     .arguments('<dir>')
@@ -12,12 +12,40 @@ program.version('0.0.1', '-v, --version')
     .action(function(dir){
 		if (!fs.existsSync(dir)){
 			fs.mkdirSync(dir);
+			inquirer.prompt([
+				{
+					type: 'list',
+					name: 'siteType',
+					message: 'What type of site are you building?',
+					choices: ['Small Site'],
+					when: function(answers) {
+						return answers.comments !== 'Nope, all good!';
+					}
+				}
+			]).then(answers => {
+				console.log(answers.siteType);
+				// TODO: copy to dir
+			});
+		}else{
+			fs.readdir(dir, function(err, files) {
+				if (!files.length) {
+					inquirer.prompt([
+						{
+							type: 'confirm',
+							name: 'useExistingDir',
+							message: 'The directory already exists but is empty. Do you want to use this directory?',
+							default: false
+						}
+					]).then(answers => {
+						if(answers.useExistingDir){
+							// TODO: copy to dir
+						}
+					});
+				}else{
+					console.error(colors.red('Folder not empty! Please empty the directory or use a different name.'));
+				}
+			});
 		}
-		console.log('sites');
-		co(function *(){
-			var siteType = yield prompt('Type of site: ');
-        	console.log(siteType, dir);
-			process.stdin.pause();
-		})
+
     })
 	.parse(process.argv);
